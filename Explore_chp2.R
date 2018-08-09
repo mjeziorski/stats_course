dbdef533439975648247eba50d55bd804af39106
Fernando A. Barrios <fabarrios@users.noreply.github.com>
2018-08-08 20:10:37 -0500

    Update Explore_chp2.R

--- Explore_chp2.R
+++ Explore_chp2.R
@@ -6,9 +6,16 @@ library(tidyverse)
 wcgs <- read_csv("DataRegressBook/wcgs.csv")
 #
 wcgs
-# to see the variables and see some of the variable names
-summary(wcgs$sbp)
+glimpse(wcgs)
+# freq() will show the variables that are cherecters and will make a bar plot of the frequencies
+freq(wcgs)
+profiling_num(wcgs)
+# will show all the numerical values
+# to see the variables and see some of the variable names using an R original function
 summary(wcgs)
+summary(wcgs$sbp)
+#
+
 # to estimate the (max - min)/#bins
 # #bins 1+3.3log(3144) ~ 13 so using #bins 15
 # binswith 8.8 -> 9
@@ -18,3 +25,9 @@ boxplot(wcgs$sbp)
 # ggplot(data = wcgs) + geom_bar(mapping = aes(x = sbp, fill = weight))
 #
 # ggplot(data = wcgs) + geom_bar(mapping = aes(x = sbp, fill = weight), position = "dodge")
+ggplot(data = wcgs) + geom_qq(aes(sample = sbp))
+# ggplot(data = wcgs) + geom_qq(aes(sample = 1:310), dist = qt, dparam = list(df=5)) sample, sbp ..theoretical..
+# scatter plot of the sbp data over weight 
+ggplot(data = wcgs) + geom_point(mapping = aes(x = weight, y = sbp))
+# to add a smoothed representative plot figure 2.9 
+ggplot(data = wcgs) + geom_point(mapping = aes(x = weight, y = sbp)) + geom_smooth(mapping = aes(x = weight, y = sbp))
