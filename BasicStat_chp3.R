883f6f363eb7ab2f8d4ff5ad8ae5c79784d9ba34
Fernando A. Barrios <fabarrios@users.noreply.github.com>
2018-08-28 12:30:36 -0500

    Update BasicStat_chp3.R
    A simple linear model added example from Daniel

--- BasicStat_chp3.R
+++ BasicStat_chp3.R
@@ -20,13 +20,23 @@ shapiro.test(uhat_SBP)
 # SBP by age in the HERS data Y variation with a single predictor
 #
 
+# Example of simplelinear model
+Exr33 <- read_csv(file="DataOther/EXR_C09_S03_03.csv")
+#
+plot(Exr33$QTC ~ Exr33$DOSE, pch=20)
+LinExr33 = lm(QTC ~ DOSE, data=Exr33)
+summary(LinExr33)
+abline(LinExr33, col=2)
+# Res y = 559.9 + 0.139 x
+
+
 # Contingency tables
 # frequency variables
 tab_wcgs <- table(wcgs$chd69, wcgs$arcus, dnn = c('CHD','arcus'))
 ftab_wcgs <- ftable(chd69~arcus~chd69, data= wcgs, dnn = c('CHD','arcus'))
-
+#
 library(gmodels)
 CrossTable(wcgs$chd69, wcgs$arcus, dnn = c('CHD','arcus'))
-
+#
 chisq.test(tab_wcgs)
 # chisq.test(tab_wcgs, simulate.p.value = TRUE)
