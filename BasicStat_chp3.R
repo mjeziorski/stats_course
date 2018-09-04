ad43156bad77bb8da11b4f8db4113412a650f8c4
Fernando A. Barrios <fabarrios@users.noreply.github.com>
2018-09-04 12:53:25 -0500

    Update BasicStat_chp3.R
    Added tow examples for the Daniel chap 9 for linear models (one variable)

--- BasicStat_chp3.R
+++ BasicStat_chp3.R
@@ -28,8 +28,49 @@ LinExr33 = lm(QTC ~ DOSE, data=Exr33)
 summary(LinExr33)
 abline(LinExr33, col=2)
 # Res y = 559.9 + 0.139 x
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+# Problem 9.3.4 Hospital tests for anticoagulant
+# Measures of INR (Y) from 90 subjects taking warfarin the 
+# independent variable was the hospital essays (X)
+library(tidyverse)
+Exr3.4 <- read_csv(file="DataOther/EXR_C09_S03_04.csv")
+plot(Y ~ X, data=Exr3.4, pch=20)
+LinExr3.4 <- lm(Y ~ X, data=Exr3.4)
+abline(LinExr3.4, col=2)
+# y = 0.4885 + 0.8625 x  with r^2 = 0.5067 (50 % data exp)
+summary(LinExr3.4)
+par(mfrow=c(2,2))
+plot(LinExr3.4)
+par(mfrow=c(1,1))
+#
+ggplot(data = Exr3.4, mapping = aes(x = X, y = Y)) + 
+  geom_point(na.rm = TRUE) + geom_jitter(height=0.05, width=0.05)
+#
+ggplot(data = Exr3.4, mapping = aes(x = X, y = Y)) + geom_point(na.rm = TRUE) + 
+  geom_abline(aes(intercept = 0.4885, slope = 0.8625, colour = "RED")) + 
+  geom_jitter(height=0.05, width=0.05)
+#
+# Problem 9.3.7 GFR is the most importatn parameter of 
+# renal function assesment in transplan, insulin is the
+# gold standar of GFR. Examied inverse Cystatin C as 
+# insulin GFR, use the DTPA GFR as the predictor
+# of the inverse Cystatin C
+Exr3.7 <- read_csv(file="DataOther/EXR_C09_S03_07.csv")
+plot(INVCYS ~ DTPA, data=Exr3.7, pch=20)
+LinExr3.7 = lm(INVCYS ~ DTPA, data=Exr3.7)
+summary(LinExr3.7)
+abline(LinExr3.7, col=2)
+# y = 0.1929 + 0.0063 x  with r^2 = 0.57 (57 % data exp)
+par(mfrow=c(2,2))
+plot(LinExr3.7)
+par(mfrow=c(1,1))
+#
+ggplot(data = Exr3.7, mapping = aes(x = DTPA, y = INVCYS)) + geom_point(na.rm = TRUE) + 
+  geom_abline(aes(intercept = 0.1929, slope = 0.0063, colour = "red")) + 
+  geom_jitter(height=0.05, width=0.05)
+#
 
-
+# #############################################################################
 # Contingency tables
 # frequency variables
 tab_wcgs <- table(wcgs$chd69, wcgs$arcus, dnn = c('CHD','arcus'))
