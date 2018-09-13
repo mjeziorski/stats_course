2f4d4c9b26196fb9b19a1181e9e1ddf38fdc6f6e
Fernando A. Barrios <fabarrios@users.noreply.github.com>
2018-09-13 17:17:25 -0500

    Update ContingTabl_chp3.R

--- ContingTabl_chp3.R
+++ ContingTabl_chp3.R
@@ -5,15 +5,19 @@
 # in the WCGS data set for CHD
 library(tidyverse)
 wcgs <- read_csv(file="DataRegressBook/Chap2/wcgs.csv")
-tab_wcgs <- table(wcgs$chd69, wcgs$arcus, dnn = c('CHD','arcus'))
-ftab_wcgs <- ftable(chd69~arcus~chd69, data= wcgs, dnn = c('CHD','arcus'))
+tab_arcus <- table(wcgs$chd69, wcgs$arcus, dnn = c('CHD','arcus'))
+ftab_arcus <- ftable(chd69~arcus~chd69, data= wcgs, dnn = c('CHD','arcus'))
 #
 library(gmodels)
 CrossTable(wcgs$chd69, wcgs$arcus, dnn = c('CHD','arcus'))
 #
-chisq.test(tab_wcgs)
+chisq.test(tab_arcus)
 # chisq.test(tab_wcgs, simulate.p.value = TRUE)
 
+# for the example in 3.7
+tab_agec <- ftable(agec~chd69, data=wcgs, dnn = c('CHD', 'agec'))
+chisq.test(tab_agec)
+CrossTable(wcgs$chd69, wcgs$agec, dnn = c('CHD','arcus'))
 # Fisher exact test, for small data table Chp 3 example table 3.6 Female
 # partner's HIV status by AIDS dignosis of male partner
 #
