01d95356e54a3ba247357300303fd1d72351fa23
Fernando A. Barrios <fabarrios@users.noreply.github.com>
2018-09-18 11:40:01 -0500

    Update Survival_chp3.R

--- Survival_chp3.R
+++ Survival_chp3.R
@@ -4,3 +4,5 @@
 leuk <- read_csv(file="DataRegressBook/Chap3/leuk.csv")
 survdiff(Surv(time, cens) ~ group, data=leuk)
 plot(survfit(Surv(time, cens) ~ group, data=leuk), main = "Acute Lymphoblastic Leukemia", lty = c(2,1), ylab = "Probability", xlab = "Remission time weeks", legend.text = c("Control", "6-MP"), legend.bty = "n")
+plot(survfit(Surv(time, cens) ~ group, data=leuk), main = "Acute Lymphoblastic Leukemia", lty = c(2,1))
+
