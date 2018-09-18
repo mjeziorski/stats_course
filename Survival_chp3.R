d4b09114d984fa817d324b5ce9ac659724ae85a2
Fernando A. Barrios <fabarrios@users.noreply.github.com>
2018-09-18 12:29:36 -0500

    Update Survival_chp3.R
    Including the examples from HSAUR2 from coin and exact distribution

--- Survival_chp3.R
+++ Survival_chp3.R
@@ -1,10 +1,25 @@
 #
 # Survival analysis with the ALL data
 #
+library("survival")
+library("tidyverce")
+library("coin")
+#
+data("glioma", packaage = "coin")
 leuk <- read_csv(file="DataRegressBook/Chap3/leuk.csv")
-survdiff(Surv(time, cens) ~ group, data=leuk)
+
 plot(survfit(Surv(time, cens) ~ group, data=leuk), main = "Acute Lymphoblastic Leukemia", lty = c(2,1), ylab = "Probability", xlab = "Remission time weeks", legend.text = c("Control", "6-MP"), legend.bty = "n")
 plot(survfit(Surv(time, cens) ~ group, data=leuk), main = "Acute Lymphoblastic Leukemia", lty = c(1,2) )
 plot(survfit(Surv(time, cens) ~ group, data=leuk), main = "Acute Lymphoblastic Leukemia", lty = c(1,2), ylab = "Probability", xlab = "Remission time weeks")
+survdiff(Surv(time, cens) ~ group, data=leuk)
 #
+g3 <- subset(glioma, histology == "Grade3")
+g4 <- subset(glioma, histology == "GBM")
 #
+plot(survfit(Surv(time, event) ~ group, data = g3), main = "Grade III Glioma", lty = c(2,1), ylab = "Probability", xlab = "Survival Months")
+plot(survfit(Surv(time, event) ~ group, data = g4), main = "Grade IV Glioma", lty = c(2,1), ylab = "Probability", xlab = "Survival Months")
+#
+surv_test(Surv(time, event) ~ group, data = g3, distribution = "exact")
+surv_test(Surv(time, event) ~ group, data = g4, distribution = "exact")
+#
+surv_test(Surv(time, event) ~ group | histology, data = glioma, distribution = approximate(B = 10000))
