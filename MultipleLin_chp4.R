fc9457bf68bffbf4f0acc173e539a2296b141b13
Fernando A. Barrios <fabarrios@users.noreply.github.com>
2018-09-19 23:10:15 -0500

    Update MultipleLin_chp4.R

--- MultipleLin_chp4.R
+++ MultipleLin_chp4.R
@@ -3,8 +3,13 @@
 # Chapter 4 examples. 4.1
 library(tidyverse)
 hers <- read_csv("DataRegressBook/Chap3/hersdata.csv")
-hers_nodi <- (filter(hers, diabetes == "no"))
+hers_nodi <- filter(hers, diabetes == "no")
 # Simple linear model with HERS data for women without diebetes
-boxplot(glucose ~ exercise, data = hers_nodi)
+ggplot(data = hers_nodi, mapping = aes(x = exercise, y = glucose)) + 
+  geom_boxplot(na.rm = TRUE) + facet_grid( . ~ diabetes) + geom_jitter(height = 0.15, width = 0.15)
+# The simple linear model adjust the exercise
 hers_nodi_Fit <- lm(glucose ~ exercise, data = hers_nodi)
 summary(hers_nodi_Fit)
+#
+hers_nodi_Fit2 <- lm(glucose ~ exercise + age + drinkany + BMI, data = hers_nodi)
+summary(hers_nodi_Fit2)
