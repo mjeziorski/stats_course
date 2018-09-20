2e3451fb5df4e806686e1706339da6a131e28724
Fernando A. Barrios <fabarrios@users.noreply.github.com>
2018-09-19 22:54:01 -0500

    Update MultipleLin_chp4.R

--- MultipleLin_chp4.R
+++ MultipleLin_chp4.R
@@ -1,3 +1,10 @@
 # Multiple linear linear regresion
 #
 # Chapter 4 examples. 4.1
+library(tidyverse)
+hers <- read_csv("DataRegressBook/Chap3/hersdata.csv")
+hers_nodi <- (filter(hers, diabetes == "no"))
+# Simple linear model with HERS data for women without diebetes
+boxplot(glucose ~ exercise, data = hers_nodi)
+hers_nodi_Fit <- lm(glucose ~ exercise, data = hers_nodi)
+summary(hers_nodi_Fit)
