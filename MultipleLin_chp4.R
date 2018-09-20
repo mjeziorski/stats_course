3d149ad4628ddf0207b76e4ee4972e517c973f76
Fernando A. Barrios <fabarrios@users.noreply.github.com>
2018-09-19 23:26:52 -0500

    Update MultipleLin_chp4.R

--- MultipleLin_chp4.R
+++ MultipleLin_chp4.R
@@ -7,9 +7,9 @@ hers_nodi <- filter(hers, diabetes == "no")
 # Simple linear model with HERS data for women without diebetes
 ggplot(data = hers_nodi, mapping = aes(x = exercise, y = glucose)) + 
   geom_boxplot(na.rm = TRUE) + facet_grid( . ~ diabetes) + geom_jitter(height = 0.15, width = 0.15)
-# The simple linear model adjust the exercise
+# The simple linear model adjust the exercise like in table 4.1
 hers_nodi_Fit <- lm(glucose ~ exercise, data = hers_nodi)
 summary(hers_nodi_Fit)
-#
+# and for obtaining the table 4.2
 hers_nodi_Fit2 <- lm(glucose ~ exercise + age + drinkany + BMI, data = hers_nodi)
 summary(hers_nodi_Fit2)
